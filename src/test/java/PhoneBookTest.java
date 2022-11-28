965d8002845574847faf86cf547b2dc4e14804e7
Григорий Федорущенко <Gfedorushenko@mail.ru>
2022-11-28 18:14:21 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,12 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class PhoneBookTest {
+    @Test
+    public void addTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertTrue(phoneBook.add("Vova", "+799977777777"));
+    }
 }
